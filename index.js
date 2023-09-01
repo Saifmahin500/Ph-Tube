7c064e91e4de5a7ff014a9a787a8a195ac782c49
saifmahin500 <saifmahin500@gmail.com>
2023-09-02 02:18:24 +0600

    added my all code

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const handleLoadVideo = async (categoryId) => {
         const time =video?.others.posted_date;
         const hours = Math.floor(time / (60 * 60));
         const minutes = Math.floor((time % (60 * 60)) / 60);
-        console.log(video);
+        // console.log(video);
         const div = document.createElement('div')
         div.innerHTML = `
         <div class="card h-[400px] bg-base-100 shadow-xl">
@@ -76,7 +76,19 @@ const handleLoadVideo = async (categoryId) => {
     }
 }
 
-  
+    const sortViewContainer = document.getElementById('sort-view')
+    const handleSortView = async () => {
+        const res = await fetch('https://openapi.programming-hero.com/api/videos/category/1000')
+        const data = await res.json()
+        const finalView3 = data.data;
+        // console.log(finalView3)
 
+        const sortNumberView =  finalView3.sort();
+        console.log(sortNumberView);
+
+
+    }
+  
+handleSortView();
 handleLoadVideo(1000);
 handleCategory();

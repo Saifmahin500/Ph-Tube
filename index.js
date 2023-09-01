2a4cd0299c9679cb8787a4225736ea366b42790a
saifmahin500 <saifmahin500@gmail.com>
2023-09-01 22:27:33 +0600

    added my all code

--- index.js
+++ index.js
@@ -25,28 +25,58 @@ const handleLoadVideo = async (categoryId) => {
     const cardContainer = document.getElementById('card-container')
     cardContainer.innerHTML = "";
     finalData2?.forEach(video => {
+
+        const time =video?.others.posted_date;
+        const hours = Math.floor(time / (60 * 60));
+        const minutes = Math.floor((time % (60 * 60)) / 60);
         console.log(video);
         const div = document.createElement('div')
         div.innerHTML = `
         <div class="card h-[400px] bg-base-100 shadow-xl">
-                <figure>
-                <img class="w-[300px] h-[200px]" src=${video?.thumbnail} alt="Shoes" /></figure>
+        <figure class="relative"><img class="h-[200px] w-full" src="${video?.thumbnail}"/>
+        <p class="absolute bottom-4  right-4 px-2 py-1 rounded-lg bg-black text-white">${time?` ${hours} hrs ${minutes} min ago` : '<p> </p>'
+    }  </p >
+        </figure >
                 <div class="card-body">
-                  <h2 class="card-title">${video?.title}</h2>
+                <div class="flex gap-4">
+                     <div>
+                <img class="w-14 h-14 rounded-full" src="${video?.authors[0]?.profile_picture}" />
+            </div>
+            <div class="flex-1">
+                <h1 class="font-bold"> ${video.title} </h1>
+                <div class="flex gap">
+                    <p>${video?.authors[0]?.profile_name}</p>
+                    <p>${video?.authors[0].verified ? '<img src="image/fi_10629607.png">' : '<p> </p>'} </p>
                 </div>
-
-                <h4>Total view : ${ video.others.views} </h4>
-              </div>
+                <p> ${video?.others?.views}views</p>
+            </div>
+        </div>
+    </div>
         
         
         `
         cardContainer.appendChild(div);
     })
     
-
+    const drawingContainer = document.getElementById('drawing-btn')
+    if (finalData2.length === 0) {
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <div class="flex justify-center items-center flex-col gap-4">
+        <img src="image/Icon.png" alt="">
+            <h1 class="text-5xl font-bold text-center"> Oops!! Sorry, There is no <br> content here </h1>
+        </div>
+        
+        `
+        drawingContainer.appendChild(div)
+        
+    }
+    else{
+        drawingContainer.innerHTML = "";
+    }
 }
 
+  
 
-
-handleLoadVideo();
+handleLoadVideo(1000);
 handleCategory();
